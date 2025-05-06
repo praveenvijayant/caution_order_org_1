4ffa4272139cacfa0b0008c5220100f8c4e338ab
praveenvijayant <110561052+praveenvijayant@users.noreply.github.com>
2025-05-06 07:39:34 +0530

    12

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Text(
+          'Railway Caution Viewer (Landscape UI Here)',
+          style: TextStyle(color: Colors.white, fontSize: 18),
+        ),
+      ),
+    ),
+  ));
+}
