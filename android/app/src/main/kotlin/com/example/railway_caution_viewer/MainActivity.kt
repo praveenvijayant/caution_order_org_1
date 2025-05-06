e18c4808cc782a741be16504461b8e076c280eef
praveenvijayant <110561052+praveenvijayant@users.noreply.github.com>
2025-05-06 07:12:57 +0530

    2

--- android/app/src/main/kotlin/com/example/railway_caution_viewer/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/railway_caution_viewer/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.railway_caution_viewer
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
